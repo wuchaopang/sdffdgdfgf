7c1fa7b0408475827b2c3815e82cc6fad9def574
wuchaopang <20141105049wch@mac23.local>
2016-06-23 11:40:01 +0800

    jisuanjisuan
    通过程序可以实现数字之间的加法运算，并且通过实现加法运算的代码可以做出减法等后续运算，代码的实现过程中主要用到的是条件语句，通过条件的控制，可以
    完成相应操作

--- sdffdgdfgf/ViewController.swift
+++ sdffdgdfgf/ViewController.swift
@@ -9,6 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func dengyu(sender: AnyObject) {
+        switch flag{
+        case 1:
+            temp = temp + (Int)(xianshi.text!)!
+            xianshi.text = "\(temp)"
+            break
+        default:
+            break
+            
+        
+    }
+    }
+    @IBAction func add(sender: AnyObject) {
+        if flag==0
+        {
+            temp = (Int)(xianshi.text!)!
+            xianshi.text=""
+        }
+        flag = 1
+
+
+    }
+    @IBAction func shuzi4(sender: AnyObject) {
+        xianshi.text!=xianshi.text! + "\(4)"
+
+    }
+   
+    @IBAction func shuzi3(sender: AnyObject) {
+        xianshi.text!=xianshi.text! + "\(3)"
+
+    }
+    @IBAction func shuzi2(sender: AnyObject) {
+        xianshi.text!=xianshi.text! + "\(2)"
+
+    }
+    @IBAction func shizi1(sender: AnyObject) {
+        xianshi.text!=xianshi.text! + "\(1)"
+
+    }
+    @IBOutlet weak var xianshi: UILabel!
+    var flag:Int = 0
+    var temp = 0
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
